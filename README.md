e3523f336ba3e9af83fc779d6724a4f8ddbeb239
Ariadna Eyzaguirre <ariadna.eyzaguirre@gmail.com>
2016-10-12 15:28:27 -0500

    v0.0.2

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ Un elemento en el HTML con el mensaje "Añadir una lista", que al dar click mues
 ![version-01-1](http://2.1m.yt/k_6IQjE.png)
 
 ![version-01-2](http://2.1m.yt/rWwe0be.png)
+
+## Versión 0.0.2
+Mostrar en el HTML, el texto ingresado al dar click en el botón de "Guardar" del formulario (como si fuera título de la lista).
+Debajo del título, mostrar el mensaje clickeable de "Añadir una tarjea".
+![]()
+![]()
--- js/app.js
+++ js/app.js
@@ -1,17 +1,38 @@
-window.addEventListener("load", function(){
-    
+window.addEventListener("load",trello);
+
+function trello(){
     var contenedor = document.getElementById("contAll");
     var subContenedor = document.getElementById("contList");
     var spanList = document.getElementById("spanList");
     var newForm = document.getElementById("newForm");
-    var newList = document.getElementById("inputList");
+    var nameList = document.getElementById("inputList");
     var btnSave = document.getElementById("buttonSave");
     
     function apareceForm(){
         spanList.style.display="none";
-        newForm.style.display="block";        
+        newForm.style.display="block";
+        
+        nameList.focus();
     }
     
-    spanList.addEventListener("click",apareceForm);
+    btnSave.addEventListener("click", function(e){
+        e.preventDefault();
+
+        newForm.style.display="none";
+
+        var tittleList = document.createElement("span");
+        var enlaceAgregar =  document.createElement("a");
+
+        tittleList.classList.add("d-block");
+        enlaceAgregar.classList.add("d-block");
+
+        tittleList.innerHTML = nameList.value;
+        enlaceAgregar.innerHTML = "Agregar nueva tarea..."
+
+        subContenedor.appendChild(tittleList);
+        subContenedor.appendChild(enlaceAgregar);
+        nameList.value = "";
+    });
     
-});
+    spanList.addEventListener("click",apareceForm);    
+}
